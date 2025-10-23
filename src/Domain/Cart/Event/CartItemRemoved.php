2eccba0f53372594d4ee7909a85e82276f96bc97
Melissa Feo La Cruz Pérez <mfeo@gestazion.com>
2025-10-24 00:20:03 +0100

    feat(cart): add CartItemRemoved domain event

---
+++ src/Domain/Cart/Event/CartItemRemoved.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Cart\Event;
+
+use App\Domain\Cart\ValueObject\CartId;
+use App\Domain\Cart\ValueObject\CartItemId;
+use App\Domain\Cart\ValueObject\ProductId;
+use App\Domain\Shared\Event\DomainEvent;
+
+final readonly class CartItemRemoved extends DomainEvent
+{
+    public function __construct(
+        private CartId $cartId,
+        private CartItemId $cartItemId,
+        private ProductId $productId,
+    ) {
+        parent::__construct();
+    }
+
+    public static function create(
+        CartId $cartId,
+        CartItemId $cartItemId,
+        ProductId $productId,
+    ): self {
+        return new self($cartId, $cartItemId, $productId);
+    }
+
+    public function cartId(): CartId
+    {
+        return $this->cartId;
+    }
+
+    public function cartItemId(): CartItemId
+    {
+        return $this->cartItemId;
+    }
+
+    public function productId(): ProductId
+    {
+        return $this->productId;
+    }
+
+    public function eventName(): string
+    {
+        return 'cart.item.removed';
+    }
+
+    public function toPrimitives(): array
+    {
+        return [
+            'cart_id' => $this->cartId->value(),
+            'cart_item_id' => $this->cartItemId->value(),
+            'product_id' => $this->productId->value(),
+            'occurred_on' => $this->occurredOn()->format(\DateTimeInterface::RFC3339),
+        ];
+    }
+}
